5a9e0d7be6003fddf614153f73343dd77824518b
nivedha <nivedha.gs6@gmail.com>
2020-07-05 17:39:02 +0530

    mysql port updated

--- model/database.js
+++ model/database.js
@@ -3,8 +3,7 @@ const connect= mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'studentdb',
-    port:'3307'
+    database: 'studentdb'
   });
 
 
